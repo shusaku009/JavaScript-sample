bb2fbdd764f35126ea7aca289d97d1db26ede841
shu009 <105494980+shusaku009@users.noreply.github.com>
2023-09-22 23:46:56 +0000

    Update 2 files

--- src/index.mjs
+++ src/index.mjs
@@ -7,4 +7,13 @@ document.getElementById("app").innerHTML = `
 const total = 42 + 42;
 console.log(total);
 
+const foo = null;
+console.log(foo);
+
+function fn(){
+    const undefined = "独自の未定義値";
+    console.log(undefined);
+}
+fn();
+
 // 
