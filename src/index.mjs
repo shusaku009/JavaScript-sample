1016246c59338b48464d56e43425e3fe4210e546
shu009 <105494980+shusaku009@users.noreply.github.com>
2023-09-23 10:03:19 +0000

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -88,3 +88,24 @@ function sum(...values) {
 const x = 1, z = 10;
 let y;
 console.log(sum(x, y, z));
+
+/**
+ * 数値を合計した値を返します。
+ * 1つ以上の数値と共に呼び出す必要があります。
+ * @param {...number} values
+ * @returns {number}
+ */
+function sum(...values) {
+    return values.reduce((total, value) =>{
+        // 値がNumber型でない場合に、例外を投げる
+        if (typeof value !== "number") {
+            throw new Error(`${value}はNumber型ではありません`);
+        }
+        return total + Number(value);
+    }, 0);
+}
+const x = 1, z = 10;
+let y;
+console.log(x, y, z);
+console.log(sum(x, y, z));
+
