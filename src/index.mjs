a357eb908700f43a434b36e961f3ec2ce357b112
shu009 <105494980+shusaku009@users.noreply.github.com>
2023-09-24 06:56:31 +0000

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -248,3 +248,14 @@ function fn() {
 // 関数`fn`を`Func`変数に代入している
 const myFunc = fn;
 myFunc();
+
+// factorialは関数の外から呼び出せる名前
+// innerFactは関数の外から呼び出せない名前
+const factorial = function innerFact(n) {
+    if (n === 0) {
+        return 1;
+    }
+    // innerFactを再帰的に呼び出している
+    return n * innerFact(n - 1);
+};
+console.log(factorial(3));
