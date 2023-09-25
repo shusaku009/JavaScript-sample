5f16f21762d0d0fd4b3debcd4941c30a7f581e37
shu009 <105494980+shusaku009@users.noreply.github.com>
2023-09-25 07:01:13 +0000

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -443,3 +443,23 @@ while (x < 10) {
 }
 console.log(`ループ終了後のxの値: ${x}`);
 
+const x = 1000;
+do {
+    console.log(x);
+} while (x < 10);
+
+let total = 0;
+for (let i = 0; i < 10; i++) {
+    total += i + 1;
+}
+console.log(total);
+
+function sum(numbers) {
+    let total = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        total += numbers[i];
+    }
+    return total;
+}
+
+console.log(sum([1, 2, 3, 4, 5]));
