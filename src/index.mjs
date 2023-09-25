a68914b7b308b33741de2137563d9fb7ae8ae659
shu009 <105494980+shusaku009@users.noreply.github.com>
2023-09-25 07:40:07 +0000

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -616,3 +616,27 @@ console.log(obj);
 
 const obj = new Object();
 console.log(obj);
+
+const obj = {
+    key: "value"
+};
+console.log(obj.key);
+console.log(obj["key"]);
+
+const obj = {
+    key: "value",
+    123: 456,
+    "my-key": "my-value"
+};
+
+console.log(obj["key"]);
+console.log(obj[123]);
+console.log(obj["123"]);
+console.log(obj["my-key"]);
+
+const languages = {
+    ja: "日本語",
+    en: "英語",
+};
+const myLang = "ja";
+console.log(languages[myLang]);
