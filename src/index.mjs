23b75197412918f19be1987c6074fd618f4b0d43
shu009 <105494980+shusaku009@users.noreply.github.com>
2023-09-25 06:57:14 +0000

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -434,3 +434,12 @@ switch (version) {
         console.log("知らないバージョンです");
         break;
 }
+
+let x = 0;
+console.log(`ループ開始前のxの値: ${x}`);
+while (x < 10) {
+    console.log(x);
+    x += 1;
+}
+console.log(`ループ終了後のxの値: ${x}`);
+
