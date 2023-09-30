7236c8fba9805a703d290a5f03d8ab1ac2e9a689
shu009 <105494980+shusaku009@users.noreply.github.com>
2023-09-30 02:05:54 +0000

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -1321,3 +1321,18 @@ const abArray = ["a"].concat("b");
 console.log(abArray);
 const abcArray = abArray.concat("c");
 console.log(abcArray);
+
+const ECMAScriptVersions = [
+    { name: "ECMAScript 1", year: 1997 },
+    { name: "ECMAScript 2", year: 1998 },
+    { name: "ECMAScript 3", year: 1999 },
+    { name: "ECMAScript 5", year: 2009 },
+    { name: "ECMAScript 5.1", year: 2011 },
+    { name: "ECMAScript 2015", year: 2015 },
+    { name: "ECMAScript 2016", year: 2016 },
+    { name: "ECMAScript 2017", year: 2017 }
+];
+const versionNames = ECMAScriptVersions
+    .filter(ECMAScript => ECMAScript.year <= 2000)
+    .map(ECMAScript => ECMAScript.name);
+console.log(versionNames);
