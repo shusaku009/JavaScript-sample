15ec6c9ff140d2f6a4ef803ecd0b7ffd9ebeddf1
shu009 <105494980+shusaku009@users.noreply.github.com>
2023-09-30 01:47:12 +0000

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -1262,3 +1262,11 @@ const newArray = array.map((currentValue, index, array) => {
 });
 console.log(newArray);
 console.log(array === newArray);
+
+const array = [1, 2, 3];
+const newArray = array.filter((currentValue, index, array) => {
+    return currentValue % 2 === 1;
+});
+console.log(newArray);
+console.log(array === newArray);
+
