733a2555ff3eec425fd1037341156aacf64edc58
shu009 <105494980+shusaku009@users.noreply.github.com>
2023-10-01 08:38:03 +0000

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -1629,3 +1629,42 @@ const codePointOfあ = "あ".codePointAt(0);
 const hexOfあ = codePointOfあ.toString(16);
 console.log(hexOfあ);
 console.log("\u{3042}");
+
+function convertCodeUnits(str) {
+    const codeUnits = [];
+    for (let i = 0; i < str.length; i++) {
+        codeUnits.push(str.charCodeAt(i).toString(16));
+    }
+    return codeUnits;
+}
+function convertCodePoints(str) {
+    return Array.from(str).map(char => {
+        return char.codePointAt(0).toString(16);
+    });
+}
+
+const str = "アオイ"
+const codeUnits = convertCodeUnits(str);
+console.log(codeUnits);
+const codePoints = convertCodePoints(str);
+console.log(codePoints);
+
+function convertCodeUnits(str) {
+    const codeUnits = [];
+    for (let i = 0; i < str.length; i++) {
+        codeUnits.push(str.charCodeAt(i).toString(16));
+    }
+    return codeUnits;
+}
+function convertCodePoints(str) {
+    return Array.from(str).map(char => {
+        return char.codePointAt(0).toString(16);
+    });
+}
+
+const str = "リンゴ🍎";
+const codeUnits = convertCodeUnits(str);
+console.log(codeUnits);
+const codePoints = convertCodePoints(str);
+console.log(codePoints);
+
