03b24953cff3c1a51a4300fad69ca66790ff48d3
shu009 <105494980+shusaku009@users.noreply.github.com>
2023-10-03 10:44:36 +0000

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -1746,3 +1746,21 @@ function fnA() {
 function fnB() {
     let x;
 }
+
+{
+    const x = 1;
+    console.log(x);
+}
+cosnole.log(x);
+
+if (true) {
+    const x = "inner";
+    console.log(x);
+}
+console.log(x);
+
+const array = [1, 2, 3, 4, 5];
+for (const element of array) {
+    console.log(element);
+}
+console.log(element);
