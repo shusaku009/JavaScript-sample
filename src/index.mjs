96dcc8e0d4d700131f6466e92b01f00dda3deb00
shu009 <105494980+shusaku009@users.noreply.github.com>
2023-10-03 10:42:48 +0000

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -1723,3 +1723,26 @@ console.log(stringWraper.valueOf());
 
 const str = "文字列";
 const stringWraper = new String("文字列");
+
+function fn() {
+    const x = 1;
+    console.log(x);
+}
+fn();
+console.log(x);
+
+function fn(arg) {
+    console.log(arg);
+}
+fn(1);
+console.log(arg);
+
+let a;
+let a;
+
+function fnA() {
+    let x;
+}
+function fnB() {
+    let x;
+}
