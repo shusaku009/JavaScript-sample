6121b5392f621f8adf8c14a16d1a533213b66cb6
shu009 <105494980+shusaku009@users.noreply.github.com>
2023-10-06 11:03:30 +0000

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -1969,3 +1969,24 @@ const newCountUp = createCounter();
 console.log(countUp === newCountUp);
 console.log(countUp());
 console.log(newCountUp());
+
+const createCounter = () => {
+    let privateCount = 0;
+    return () => {
+        privateCount++;
+        return `${privateCount}回目`;
+    };
+};
+const counter = createCounter();
+console.log(counter());
+console.log(counter());
+
+function greaterThan(n) {
+    return function(m) {
+        return m > n;
+    };
+}
+const greaterThan5 = greaterThan(5);
+console.log(greaterThan5(4));
+console.log(greaterThan5(5));
+console.log(greaterThan5(6));
