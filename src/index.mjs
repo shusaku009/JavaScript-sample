fb12c7bf140f761c3a36b9a870b04130b63d66c1
shu009 <105494980+shusaku009@users.noreply.github.com>
2023-10-08 10:03:08 +0000

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -2537,3 +2537,21 @@ class NumberWrapper {
 
 const numberWrapper = new NumberWrapper(1);
 console.log(numberWrapper.#value);
+
+class  ArrayWrapper {
+    constructor(array = []) {
+        this.array = array;
+    }
+    static of(...items) {
+        return new ArrayWrapper(items);
+    }
+
+    get length() {
+        return this.array.length;
+    }
+}
+const arrayWrapperA = new ArrayWrapper([1, 2, 3]);
+const arrayWrapperB = ArrayWrapper.of(1, 2, 3);
+console.log(arrayWrapperA.length);
+console.log(arrayWrapperB.length);
+
