54ad36897195be9d0e03223437233c8d48ab8fe9
shu009 <105494980+shusaku009@users.noreply.github.com>
2023-10-08 01:44:31 +0000

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -2153,3 +2153,51 @@ const sayPerson = () => {
     return say.call(person, "こんにちは");
 };
 console.log(sayPerson());
+
+"use strict";
+const Prefixer = {
+    prefix: "pre",
+    prefixArray(strings) {
+        return strings.map(function(str) {
+            return this.prefix + "-" + str;
+        });
+    }
+}
+Prefixer.prefixArray(["a", "b", "c"]);
+
+"use strict";
+const Prefixer = {
+    prefix: "pre",
+    prefixArray(strings) {
+        const callback = function(str) {
+            return this.prefix + "-" + str;
+        };
+        return strings.map(callback);
+    }
+};
+Prefixer.prefixArray(["a", "b", "c"]);
+
+"use strict";
+const Prefixer = {
+    prefix: "pre",
+    prefixArray(strings) {
+        const that = this;
+        return strings.map(function(str) {
+            return that.prefix + "-" + str;
+        });
+    }
+};
+const prefixedStrings = Prefixer.prefixArray(["a", "b", "c"]);
+console.log(prefixedStrings);
+
+"use strict";
+const Prefixer = {
+    prefix: "pre",
+    prefixArray(strings) {
+        return strings.map(function(str) {
+            return this.prefix + "-" + str;
+        }, this);
+    }
+};
+const prefixedStrings = Prefixer.prefixArray(["a", "b", "c"]);
+console.log(prefixedStrings);
