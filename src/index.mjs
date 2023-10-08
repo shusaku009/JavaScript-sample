506fe416b710e522806d4fa8637382f584c43984
shu009 <105494980+shusaku009@users.noreply.github.com>
2023-10-08 08:47:27 +0000

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -2355,3 +2355,38 @@ const numberWrapper = new NumberWrapper(1);
 console.log(numberWrapper.value);
 numberWrapper.value = 42;
 console.log(numberWrapper.value);
+
+const array = [1, 2, 3, 4, 5];
+array.length = 2;
+console.log(array.join(", "));
+array.length = 5;
+console.log(array.join(", "));
+
+class ArrayLike {
+    constructor(items = []) {
+        this._items = items;
+    }
+
+    get items() {
+        return this._items;
+    }
+
+    get length() {
+        return this._items.length;
+    }
+
+    set length(newLength) {
+        const currentItemsLength = this.items.length;
+        if (newLength < currentItemsLength) {
+            this._items = this.items.slice(0, newLength);
+        } else if (newLength > currentItemsLength) {
+            this._items = this.items.concat(new Array(newLength - currentItemsLength));
+        }
+    }
+}
+
+const arrayLike = new ArrayLike([1, 2, 3, 4, 5]);
+arrayLike.length = 2;
+console.log(arrayLike.items.join(", "));
+arrayLike.length = 5;
+console.log(arrayLike.items.join(", "));
