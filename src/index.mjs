9550e8a91de0b205beaad1ea86b8a162e6b7982b
shu009 <105494980+shusaku009@users.noreply.github.com>
2023-10-09 08:35:12 +0000

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -2794,3 +2794,17 @@ console.log(parent instanceof Parent);
 console.log(parent instanceof Child);
 console.log(child instanceof Parent);
 console.log(child instanceof Child);
+
+class MyArray extends Array {
+    get first() {
+        return this.at(0);
+    }
+
+    get last() {
+        return this.at(-1);
+    }
+}
+const array = MyArray.from([1, 2, 3, 4, 5]);
+console.log(array.length);
+console.log(array.first);
+console.log(array.last);
