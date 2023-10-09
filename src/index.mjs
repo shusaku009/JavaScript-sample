71386d15854872ffa55ace001bfcd6a5462d9360
shu009 <105494980+shusaku009@users.noreply.github.com>
2023-10-09 07:39:01 +0000

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -2692,3 +2692,19 @@ class Child extends Parent {
         super(...args);
     }
 }
+
+class Parent {
+    constructor() {
+        this.name = "parent";
+    }
+}
+class Child extends Parent {
+    constructor() {
+        super();
+        this.name = "Child";
+    }
+}
+const parent = new Parent();
+console.log(parent.name);
+const child = new Child();
+console.log(child.name);
