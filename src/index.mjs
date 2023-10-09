1eae515059f130a0e730a77eea45692032e66232
shu009 <105494980+shusaku009@users.noreply.github.com>
2023-10-09 15:00:41 +0000

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -2940,3 +2940,32 @@ setTimeout(() => {
     taskAsync();
 }, 1000);
 taskB();
+
+function taskA() {
+    console.log("タスクAを実行 at " + Date.now());
+}
+function taskB() {
+    console.log("タスクBを実行 at " + Date.now());
+}
+function taskAsync() {
+    console.log("非同期のタスクを実行 at " + Date.now());
+}
+function blockTime(timeout) {
+    const startTime = Date.now();
+    while (true) {
+        const diffTime = Date.now() - startTime;
+        if (diffTime >= timeout) {
+            return;
+        }
+    }
+}
+
+const startTime = Date.now();
+taskA();
+setTimeout(() => {
+    const endTime = Date.now();
+    taskAsync();
+    console.log(`非同期処理のコールバックが呼ばれるまで${endTime - startTime}ミリ秒かかりました`);
+}, 10);
+blockTime(1000);
+taskB();
