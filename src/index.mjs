dfab69c6c5e390545db9e6080dca16e1877c1441
shu009 <105494980+shusaku009@users.noreply.github.com>
2023-10-13 09:13:41 +0000

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -3122,3 +3122,14 @@ const promise = new Promise((resolve) => {
     resolve();
 });
 console.log("2. 同期的な処理が実行されました");
+
+const rejectedPromise = Promise.reject(new Error("エラー"));
+
+const rejectedPromise = new Promise((resolve, reject) => {
+    reject(new Error("エラー"));
+});
+
+Promise.reject(new Error("エラー")).catch(() => {
+    console.log("2. コールバック関数が実行されました");
+});
+console.log("1. 同期的な処理が実行されました");
