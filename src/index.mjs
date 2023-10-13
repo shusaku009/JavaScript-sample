68af2a0f5b5202ae21ed75d1752b460c2c575c86
shu009 <105494980+shusaku009@users.noreply.github.com>
2023-10-13 14:20:57 +0000

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -3340,3 +3340,48 @@ fetchedPromise.then(([responseA, responseB]) => {
 }).catch(error => {
     console.log(error);
 });
+
+function delay(timeoutMs) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(timeoutMs);
+        }, timeoutMs);
+    });
+}
+
+const racePromise = Promise.race([
+    delay(1),
+    delay(32),
+    delay(64),
+    delay(128)
+]);
+racePromise.then(value => {
+    console.log(value);
+});
+
+function timeout(timeoutMs) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject(new Error(`Timeout: ${timeoutMs}ミリ秒経過`));
+        }, timeoutMs);
+    });
+}
+function dummyFetch(path) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (path.startsWith("/resource")) {
+                resolve({ body: `Response body of ${path}` });
+            } else {
+                reject(new Error("NOT FOUND"));
+            }
+        }, 1000 * Math.random());
+    });
+}
+Promise.race([
+    dummyFetch("/resource/data"),
+    timeout(500),
+]).then(response => {
+    console.log(response.body);
+}).catch(error => {
+    console.log(error.message);
+});
