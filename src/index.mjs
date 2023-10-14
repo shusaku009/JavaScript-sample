713fbe8997501c27b2ccd4a5790af390ee0051b5
shu009 <105494980+shusaku009@users.noreply.github.com>
2023-10-14 14:55:04 +0000

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -3623,4 +3623,31 @@ fetchResources(resources).then((results) => {
     console.log(results);
 });
 
-function dummyFetch(path) 
+function dummyFetch(path) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (path.startsWith("/resource")) {
+                resolve({ body: `Response body of ${path}` });
+            } else {
+                reject(new Error("NOT FOUND"));
+            }
+        }, 1000 * Math.random());
+    });
+}
+async function fetchResources(resources) {
+    const results = [];
+    console.log("1. fetchResourcesを開始");
+    resources.forEach(async function(resource) {
+        console.log(`2. ${resource}の取得開始`);
+        const response = await dummyFetch(resource);
+        console.log(`5. ${resource}の取得完了`);
+        results.push(response.body);
+    });
+    console.log("3. fetchResourcesを終了");
+    return results;
+}
+const resources = ["/resource/A", "/resource/B"];
+fetchResources(resources).then((results) => {
+    console.log("4.fetchResourcesの結果を取得");
+    console.log(results);
+});
