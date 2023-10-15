879aa5661c4bc98d2d4076e1523f0bb7b7b9f290
shu009 <105494980+shusaku009@users.noreply.github.com>
2023-10-15 06:39:41 +0000

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -3845,3 +3845,18 @@ for (const value of set) {
     results.push(value);
 }
 console.log(results);
+
+const json = '{ "id": 1, "name": "js-primer" }';
+const obj = JSON.parse(json);
+console.log(obj.id);
+console.log(obj.name);
+
+const json = "[1, 2, 3]";
+console.log(JSON.parse(json));
+
+const userInput = "not json value";
+try {
+    const json = JSON.parse(userInput);
+} catch (error) {
+    console.log("パースできませんでした");
+}
