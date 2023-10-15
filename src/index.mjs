8910c5ee2d8a8dcd65d719e7f0c20083de68b9dc
shu009 <105494980+shusaku009@users.noreply.github.com>
2023-10-15 07:16:44 +0000

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -3915,3 +3915,22 @@ console.log(Date.now());
 
 console.log(now.getTime());
 console.log(now.toISOString());
+
+const date = new Date(1136214245999);
+console.log(date.toISOString());
+
+const unUTC = new Date("2006-01-02T15:04:05.999Z");
+const inLocal = new Date("2006-01-02T15:04:05.999");
+console.log(inLocal.toISOString());
+
+const date1 = new Date(2006, 0, 2, 15, 4, 5, 999);
+console.log(date1.toISOString());
+
+const ms = Date.UTC(2006, 0, 2, 15, 4, 5, 999);
+const date2 = new Date(ms);
+console.log(date2.toISOString());
+
+const invalid = new Date("");
+console.log(invalid.getTime());
+console.log(invalid.toString());
+console.log(Number.isNaN(invalid.getTime()));
