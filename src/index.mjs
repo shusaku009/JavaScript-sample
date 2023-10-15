9ab65005272673c0fcc7ef9911367be7ec0d1b23
shu009 <105494980+shusaku009@users.noreply.github.com>
2023-10-15 01:32:42 +0000

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -3710,3 +3710,59 @@ for (const [key, value] of map) {
     results.push(`${key}:${value}`);
 }
 console.log(results);
+
+const map = {};
+function has(key) {
+    return typeof map[key] !== "undefined";
+}
+console.log(has("foo"));
+console.log(has("constructor"));
+
+class ShoppingCart {
+    constructor() {
+        this.items = new Map();
+    }
+    addItem(item) {
+        const count = this.items.get(item) ?? 0;
+        this.items.set(item, count + 1);
+    }
+    getTotalPrice() {
+        return Array.from(this.items).reduce((total, [item, count]) => {
+            return total + item.price * count;
+        }, 0);
+    }
+    toString() {
+        return Array.from(this.items).map(([item, count]) => {
+            return `${item.name}:${count}`;
+        }).join(",");
+    }
+}
+const shoppingCart = new ShoppingCart();
+const shopItems = [
+    { name: "みかん", price: 100},
+    { name: "リンゴ", price: 200},
+];
+shoppingCart.addItem(shopItems[0]);
+shoppingCart.addItem(shopItems[0]);
+shoppingCart.addItem(shopItems[1]);
+
+console.log(shoppingCart.getTotalPrice());
+console.log(shoppingCart.toString());
+
+function sendPOSTRequest(url, data) {
+    fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json"},
+        body: JSON.stringify(data),
+    }).catch((error) => {
+        console.log(error);
+    });
+}
+function onLoginFormsSubmit(event) {
+    const form = event.target;
+    const data = {
+        userName: form.elements.userName,
+        password: form.elements.password,
+    };
+    sendPOSTRequest("/api/login", data)
+}
